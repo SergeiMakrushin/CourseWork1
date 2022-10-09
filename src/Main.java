b4fd3ad420dfc4df8c715e73d79beec711c102bc
Сергей <mak.sergei@ngs.ru>
2022-10-09 22:40:40 +0700

    Курсовая работа (базовый вариант)

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,62 @@
-
-public class Main {private static  Employee[] employees = new Employee[10];
+public class Main {
+    private static Employee[] employees = new Employee[10];
 
     public static void listEmployees(Employee[] information) {
         for (int i = 0; i < information.length; i++)
-            if (information[i] != null)
-            {System.out.println("Информация о сотруднике "+information[i]);}
+            if (information[i] != null) {
+                System.out.println("Информация о сотруднике " + information[i]);
+            }
     }
+
     public static int salarySum(Employee[] sulary) {
         int sum = 0;
         for (int i = 0; i < sulary.length; i++)
             if (sulary[i] != null) {
                 sum = sum + sulary[i].getSalaryEmployee();
-            }return sum;
+            }
+        return sum;
     }
-    public static void mediumSulary(Employee[] information,int sum) {
+
+    public static void mediumSulary(Employee[] information, int sum) {
         int b = 0;
         int medium = 0;
-        for (int i = 0; i < information.length; i++)
+        for (int i = 0; i < information.length; i++) {
             if (information[i] != null) {
                 b++;
-                medium = sum / b;
-            }    System.out.println("Средняя зарплата  = " + medium);
+            }
+        }
+        medium = sum / b;
+        System.out.println("Средняя зарплата  = " + medium);
     }
+
     public static void employeeMinSalary(Employee[] information) {
         int minSalary = 100000000;
         for (int i = 0; i < information.length; i++) {
-            if (information[i] != null)
-                if (information[i].getSalaryEmployee()<minSalary)
-                {minSalary = information[i].getSalaryEmployee();}
-        }System.out.println("Минимальная зарплата = " + minSalary);
+            if (information[i] != null) if (information[i].getSalaryEmployee() < minSalary) {
+                minSalary = information[i].getSalaryEmployee();
+            }
+        }
+        System.out.println("Минимальная зарплата = " + minSalary);
     }
+
     public static void employeeMaxSalary(Employee[] information) {
         int maxSalary = -1;
         for (int i = 0; i < information.length; i++) {
-            if (information[i] != null)
-                if (information[i].getSalaryEmployee()>maxSalary)
-                {maxSalary = information[i].getSalaryEmployee();}
-        }System.out.println("Максимальная зарплата = " + maxSalary);
+            if (information[i] != null) if (information[i].getSalaryEmployee() > maxSalary) {
+                maxSalary = information[i].getSalaryEmployee();
+            }
+        }
+        System.out.println("Максимальная зарплата = " + maxSalary);
     }
 
     public static void fullnameEmployees(Employee[] information) {
-        String name;
         for (int i = 0; i < information.length; i++)
-            if (information[i] != null)
-            {System.out.println("Ф. И. О. сотрудника: "+information[i].getFullNameEmployee());}
+            if (information[i] != null) {
+                System.out.println("Ф. И. О. сотрудника: " + information[i].getFullNameEmployee());
+            }
     }
-    public static void main (String[]args){
+
+    public static void main(String[] args) {
         System.out.println("Hello world!");
         Employee employee = new Employee("Ivanov Oleg Michailovich", 1, 30000);
         Employee employee2 = new Employee("Sidorov Ivan Olegovich", 2, 40000);
@@ -61,11 +72,10 @@ public class Main {private static  Employee[] employees = new Employee[10];
 
         listEmployees(employees);
 
-
         int sumSalary = salarySum(employees);
         System.out.println("Затраты на зарплаты в месяц = " + sumSalary);
 
-        mediumSulary(employees,sumSalary);
+        mediumSulary(employees, sumSalary);
 
         employeeMinSalary(employees);
 
